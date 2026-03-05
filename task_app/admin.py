24bd1ac54d2b7ab74a948c12ad90f02adf53452e
a-gskrv <agskrv@gmail.com>
2026-03-05 13:23:08 +0100

    Edit ModelAdmin for classes: Task, SubTask, Category. Homework 09 Albert

--- task_app/admin.py
+++ task_app/admin.py
@@ -2,6 +2,67 @@ from django.contrib import admin
 
 from task_app.models import Task, SubTask, Category
 
-admin.site.register(Task)
-admin.site.register(SubTask)
-admin.site.register(Category)
+
+@admin.register(Task)
+class BorrowAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'categories',
+        'status',
+        'deadline',
+        'created_at',
+    ]
+
+    list_filter = [
+        'categories',
+        'status',
+        'deadline',
+    ]
+
+    search_fields = [
+        'title',
+        'description',
+    ]
+
+    list_editable = ('status', 'deadline')
+
+    list_per_page = 25
+
+
+@admin.register(SubTask)
+class BorrowAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'task',
+        'status',
+        'deadline',
+        'created_at',
+    ]
+
+    list_filter = [
+        'task',
+        'status',
+        'deadline',
+    ]
+
+    search_fields = [
+        'title',
+        'description',
+    ]
+
+    list_editable = ('status', 'deadline')
+
+    list_per_page = 25
+
+
+@admin.register(Category)
+class BorrowAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+    ]
+
+    search_fields = [
+        'name',
+    ]
+
+    list_per_page = 25
